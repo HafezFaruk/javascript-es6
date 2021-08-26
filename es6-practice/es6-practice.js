353df18e94e4be5c8b0398112c1a912e8b288003
md omar faruk <hmomarfaruk1454@gmail.com>
2021-08-26 06:34:18 +0600

    javascript es6

---
+++ es6-practice/es6-practice.js
@@ -0,0 +1,195 @@
+/*
+1. let const
+2. template string
+2.a: use variable
+2.b: use object  property
+3. arrow function
+3.a. with one parameter. will return the number after dividing by 5
+3.b. with two parameters. will add two to each number and then multiply the result.
+3.c. three parameters. will multiply all three parameters
+3.d. with two parameters but it will be a multi line arrow function. will add two to each number and then multiply the result.
+make sure you return the result
+4. [home work]: javascript function declaration vs arrow function
+5. array of numbers. and then map to get each elements multiplied by 5. must use arrow function
+6. [challenging] numbers of array. get odd numbers by using filter with arrow function
+7. [challenging] array of objects (e.g. products). use find to get the object with price 5000.
+8. You have an object. Now use destructing to create a simple variable of any property of the object.
+9. [challenging] You have an array. Now use destructing to create a simple (or more) variable to get the third element of the array in a variable called 'three'.
+10. [optional] just a write a function with three parameters. and the last parameter will have a default parameter with value 7.
+this function will take three parameters and will return the sum of all the three parameters.
+*/
+
+/* 
+১. কখন const আর কখন let দিয়ে ভেরিয়েবল ডিক্লেয়ার করতে হয় সেটা তোমাকে জানতেই হবে। তুমি নিজে নিজে একটা const দিয়ে আরেকটা let দিয়ে ভেরিয়েবল ডিক্লেয়ার করে ফেলো।
+---------------------------*/
+// let and const
+const myCuntry = 'Bangladesh';
+console.log(myCuntry);
+
+let phone = 'semsang';
+phone = 'sawmi';
+console.log(phone);
+
+/*
+২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও। 
+--------------------------------*/
+const firstName = 'Hafez Mawlana';
+const lastName = 'Md Omar Faruk';
+const fullName = `${firstName} ${lastName}`;
+//console.log(fullName);
+
+const mobilePhone = {
+    name: 'TCL',
+    price: 3000,
+    color: 'black',
+    mudel: 'NF3'
+}
+const customar = `${fullName} My Mobile Phone Name ${mobilePhone.name} and Color ${mobilePhone.color}`;
+console.log(customar);
+
+/* 
+৩.১ একটা প্যারামিটার ওয়ালা arrow ফাংশন ডিক্লেয়ার করো। এবং সেই ফাংশনের কাজ হবে তুমি কোন ইনপুট দিলে সেই ইনপুট সংখ্যাকে ৫ দিয়ে ভাগ করে আউটপুট দিবে। 
+--------------------------------------*/
+const fivedivition = num => num / 5;
+const value = fivedivition(30);
+console.log(value);
+/*  
+৩.২ তুমি দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে। সেই ফাংশনের ভিতরে কাজ হবে। প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো 
+--------------------------------------*/
+const add = (x, y) => (x + 2) * (y + 2);
+console.log(add(2, 2));
+/*  
+৩.৩ এইবার তিনটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন ডিক্লেয়ার করো। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটারকে গুণ করে সেই রেজাল্ট রিটার্ন করবে। 
+--------------------------------------*/
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+const result = multiply(4, 12, 3);
+console.log(result);
+/* 
+৩.৪ এইবার দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন নিবে। ওই arrow ফাংশনটা হবে অনেকগুলা লাইনের। সেখানে প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো। 
+---------------------------------------*/
+const doMath = (x, y) => {
+    const sum1 = x + 2;
+    const sum2 = y + 2;
+    const sum3 = sum1 * sum2;
+    return sum3;
+}
+
+const total = doMath(2, 2);
+console.log(total);
+
+/*
+৪. [হোম ওয়ার্ক] একটু গুগলে সার্চ দাও: javascript function declaration vs arrow function তারপর কয়েকটা আর্টিকেল পড়ে বুঝার চেষ্টা করো। 
+---------------------------------------*/
+
+/* 
+৪.৫. [এক্সট্রা আরেকটা হোম ওয়ার্ক। এইটা ভিডিওতে বলা নাই]: জাভাস্ক্রিপ্ট এর var, let, const এর মধ্যে প্রার্থক্য কি? 
+--------------------------------------*/
+/* 
+
+/* 
+৫. অনেকগুলা সংখ্যার একটা array হবে। তারপর তোমার কাজ হবে array এর উপরে map ইউজ করে। প্রত্যেকটা উপাদানকে ৫ দিয়ে গুন্ করে গুনফল আরেকটা array হিসেবে রাখবে। পুরা কাজটা এক লাইনে হবে।
+--------------------------------------*/
+const arrNum = [23, 4, 43,].map(x => x * 5);
+console.log(arrNum);
+/* 
+৬. [ চ্যালেঞ্জিং। গুগলে সার্চ দিয়ে বের করো ] অনেকগুলা সংখ্যার একটা array থেকে শুধু বিজোড় সংখ্যা বের করে নিয়ে আসার জন্য filter ইউজ করো 
+-----------------------------*/
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+let odds = arr.filter(n => n % 2)
+console.log(odds)
+/*
+৭. একটা array এর মধ্যে অনেকগুলা অবজেক্ট আছে। সেখানে যেই অবজেক্ট এর price আছে ৫০০০ টেক্কা সেই অবজেক্টকে find দিয়ে বের করো। 
+-----------------------------------*/
+const products = [
+    { name: 'water bottle', price: 50, color: 'yellow' },
+    { name: 'mobile phone', price: 5000, color: 'black' },
+    { name: 'smart watch', price: 3000, color: 'black' },
+    { name: 'sticky note', price: 30, color: 'pink' },
+    { name: 'water glass', price: 3, color: 'white' }
+];
+
+const expensive = products.find(product => product.price == 5000);
+console.log(expensive);
+/*
+৭.৫ [এক্সট্রা] জাভাস্ক্রিপ্ট এ array এর map, forEach, filter, find কোনটা দিয়ে কি হয়। সেটার একটা সামারি লিখে ফেলো। 
+------------------------------------*/
+
+/*  
+৮. সিম্পল একটা জাভাস্ক্রিপ্ট অবজেক্ট এর কোন একটা প্রোপার্টিকে ভেরিয়েবল হিসেবে ডিক্লেয়ার করার জন্য destructuring ইউজ করো। 
+ ------------------------------------*/
+const gropOfCompany = {
+    name: 'Akij Grop',
+    documentOfCompany: {
+        registerNumber: 2342,
+        founder: 'Sheikh Akijuddin',
+        address: 'khulna',
+    }
+};
+const { name } = gropOfCompany
+console.log(name);
+
+/*
+৯. [চ্যালেঞ্জিং] array এর destructuring করবে আর সেটা করার জন্য তুমি এরে এর থার্ড পজিশন এর উপাদান কে destructuring করে 'three' নামক একটা ভেরিয়েবল এ রাখবে।
+-------------------------------------*/
+const [x, y, z] = [2, 3, 4];
+const tree = z;
+console.log(tree);
+/*
+১০. তিনটা প্যারামিটার ওয়ালা একটা ফাংশন লিখবে। যেই ফাংশনের কাজ হবে তিনটা প্যারামিটার নিয়ে সেই তিনটা প্যারামিটার এর যোগ করে যোগফল রিটার্ন করবে। আর থার্ড প্যারামিটার এর একটা ডিফল্ট ভ্যালু থাকবে। সেটা হবে ৭।
+-----------------------------------=-*/
+const getMath = (x, y, z = 7) => {
+    const output = x + y + z;
+    return output;
+}
+const total = getMath(2, 1);
+console.log(total);
+// [অপশনাল]
+
+/*
+১১. একটা nested অবজেক্ট ডিক্লেয়ার করো (অর্থাৎ একটা অবজেক্ট এর প্রপার্টি এর মধ্যেও যে অবজেক্ট থাকতে পারে। আবার সেই অবজেক্ট এর প্রপার্টি এর মধ্যেও সে অবজেক্ট থাকতে পারে। সেই রকম একটা অবজেক্ট ডিক্লেয়ার করো। এবং যেকোন একটা প্রপার্টি এর মান একটা array হবে। জাস্ট এমন একটা অবজেক্ট )
+-------------------------------------*/
+const company = {
+    name: 'Akij Grop',
+    documentOfCompany: {
+        registerNumber: 2342,
+        founder: 'Sheikh Akijuddin',
+        address: 'khulna',
+        web: {
+            work: 'website development',
+            employee: 22,
+            framework: 'react',
+            tech: {
+                first: 'html',
+                second: 'css',
+                third: ['js', 'React', 'Bootstrap']
+            }
+        }
+    }
+};
+const { work } = company.documentOfCompany.web;
+console.log(work);
+
+/*
+১২. উপরের অবজেক্ট এ ডট এর আগে যে প্রশ্নবোধক চিহ্ন দিয়ে যে অপশনাল চেইনিং করা যায়। সেটা একটু প্রাকটিস করো।
+-------------------------------------*/
+const gropOfCompany = {
+    name: 'Akij Grop',
+    documentOfCompany: {
+        registerNumber: 2342,
+        founder: 'Sheikh Akijuddin',
+        address: 'khulna',
+        web: {
+            work: 'website development',
+            employee: 22,
+            framework: 'react',
+            tech: {
+                first: 'html',
+                second: 'css',
+                third: ['js', 'React', 'Bootstrap']
+            }
+        }
+    }
+};
+
+console.log(gropOfCompany?.documentOfCompany?.web?.tech?.third[0]);
